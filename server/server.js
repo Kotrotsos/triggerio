4c370581fc344f1c9db23ef424bf08843e74b320
Marco Kotrotsos <MarcoK@marcok.lan>
2012-12-19 15:37:56 +0100

    ...

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 
-require('./routes')(app);
+var redis = require("redis"),
+       db = redis.createClient();
 
+db.on("error", function (err) {
+   console.log("Redis Error " + err);
+});
+
+require('./routes/action-routes')(app);
+
+db.incr("launched");
+db.get("launched", function (err, reply) {
+    console.log("launch: #", reply.toString());
+});
 
 console.log('Trigger is now listening on 0.0.0.0:3000');
 app.listen(3000);
